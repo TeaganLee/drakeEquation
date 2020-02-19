998f8ba774b4c8bdf16104f7fe53e167726b2581
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-02-19 13:11:57 -0500

    The Real Code
    I fixed f*** up

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,46 +1,14 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner keyboard;
-        keyboard = new Scanner (System.in);
-
-        String rStar;
-        String f_p;
-        String numberOfExoplanets;
-        String f_l;
-        String f_i;
-        String f_c;
-        String L;
-
-        // Code
-
-        System.out.println("rate of star formation");
-        rStar = keyboard.nextLine();
-
-        System.out.println ("Fraction of said stars with planets");
-        f_p = keyboard.nextLine();
-
-        System.out.println("# of planets capable of supporting life");
-        numberOfExoplanets = keyboard.nextLine();
-
-        System.out.println ("Fraction of Exoplanets capable of supporting life");
-        f_l = keyboard.nextLine();
-
-        System.out.println ("fraction of that life that develops intellegence");
-        f_i = keyboard.nextLine();
-
-        System.out.println("Develops communication");
-        f_c = keyboard.nextLine();
-
-        System.out.println("Longevity of communication");
-        L = keyboard.nextLine();
-
     }
+        public static void drakeEquation() {
 
-    public static void drakeEquation(){
 /*Drake equation
 N = number of intelligent species that can communicate with us
 R∗ = 1 yr−1 (1 star formed per year, on the average over the life of the galaxy; this was regarded as conservative)
@@ -52,20 +20,52 @@ fc = 0.1 to 0.2 (10–20% of which will be able to communicate)
 L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,000,000 years)
  */
 
-int N;
-int rStar;
-double f_p;
-int numberOfExoplanets;
-double f_l;
-double f_i;
-double f_c;
-int L;
 
-N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
-System.out.println("The number of intelligent species that are out there, \n"
-        + "that can communicate with us is: " + N );
 
-    }
+            Scanner keyboard;
+            keyboard = new Scanner (System.in);
+
+            int N;
+            int rStar;
+            double f_p;
+            int numberOfExoplanets;
+            double f_l;
+            double f_i;
+            double f_c;
+            int L;
+
+            System.out.println("rate of star formation");
+            rStar = keyboard.nextInt();
+
+            System.out.println ("Fraction of said stars with planets");
+            f_p = keyboard.nextDouble();
+
+            System.out.println("# of planets capable of supporting life");
+            numberOfExoplanets = keyboard.nextInt();
+
+            System.out.println ("Fraction of Exoplanets capable of supporting life");
+            f_l = keyboard.nextDouble();
+
+            System.out.println ("fraction of that life that develops intelligence");
+            f_i = keyboard.nextDouble();
+
+            System.out.println("Develops communication");
+            f_c = keyboard.nextDouble();
+
+            System.out.println("Longevity of communication");
+            L = keyboard.nextInt();
+
+            //equation
+
+            N = (int)(rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L);
+
+            System.out.println("The number of intelligent species that are out there, \n"
+                    + "that can communicate with us is: " + N );
+
+
+
+
+        }
 
 }
